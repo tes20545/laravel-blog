ccde9ec115e9ba44326ccf1ad82eda3c4cd2471b
Suwijak Srikhao <tes.20545@gmail.com>
2023-04-04 21:39:26 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                dsadas
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}

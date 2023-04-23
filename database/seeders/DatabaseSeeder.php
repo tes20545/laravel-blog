6d9f0229ae2bd07e7051e1e21a78222494d99038
Suwijak Srikhao <tes.20545@gmail.com>
2023-04-23 22:19:23 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
-         \App\Models\Blog::factory(10)->create();
-            Team::factory(10)->create();
+        //  \App\Models\User::factory(10)->create();
+        //  \App\Models\Blog::factory(10)->create();
+            // Team::factory(10)->create();
 
          \App\Models\User::factory()->create([
              'name' => 'Test User',
              'email' => 'test@example.com',
+             'position' => 'admin',
          ]);
     }
 }
